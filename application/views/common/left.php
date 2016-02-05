a27b0ac14f27680299620355634e732491573a35
Nikhil Verma <vrmanikhil@gmail.com>
2016-02-06 02:22:24 +0530

    menu fixed

--- application/views/common/left.php
+++ application/views/common/left.php
@@ -1,15 +1,15 @@
 <div class="list-group">
                     <h4 style="text-align: center;">My Dashboard</h4>
-                    <a href="/merchant_coupons_issued" class="list-group-item side">Batches</a>
-                    <a href="/users_with_coupons" class="list-group-item side">Departments</a>
-                    <a href="/users_with_coupons" class="list-group-item side">Subjects</a>
-                    <a href="/users_with_coupons" class="list-group-item side">Add Subject</a>
-                    <a href="/users_with_coupons" class="list-group-item side">Students</a>
-                    <a href="/users_with_coupons" class="list-group-item side">Add Student</a>
+                    <a href="/batches" class="list-group-item side">Batches</a>
+                    <a href="/departments" class="list-group-item side">Departments</a>
+                    <a href="/subjects" class="list-group-item side">Subjects</a>
+                    <a href="/add_subject" class="list-group-item side">Add Subject</a>
+                    <a href="/students" class="list-group-item side">Students</a>
+                    <a href="/add_student" class="list-group-item side">Add Student</a>
                    <h4 style="text-align: center;">My Account</h4>
-                    <a href="/merchant_coupons_issued" class="list-group-item side">My Profile</a>
-                    <a href="/users_with_coupons" class="list-group-item side">Change Password</a>
+                    <a href="/user_profile" class="list-group-item side">My Profile</a>
+                    <a href="/change_password" class="list-group-item side">Change Password</a>
                     <h4 style="text-align: center;">Academic Analysis</h4>
-                     <a href="/merchant_coupons_issued" class="list-group-item side">Student Analysis</a>
-                     <a href="/users_with_coupons" class="list-group-item side">Subject Analysis</a>
+                     <a href="/student_analysis" class="list-group-item side">Student Analysis</a>
+                     <a href="/subject_analysis" class="list-group-item side">Subject Analysis</a>
                 </div>
